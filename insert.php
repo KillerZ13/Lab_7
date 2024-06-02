8b7f92a3835fe13e63626fd23f3df8d0c03070bf
TS.Hazim <killerzlife13@gmail.com>
2024-06-02 21:35:32 +0800

    initial

--- insert.php
+++ insert.php
@@ -9,8 +9,19 @@ $db = $database->getConnection();
 // Create an instance of the User class
 $user = new User($db);
 
-// Register the user using POST data
-$user->createUser($_POST['matric'], $_POST['name'], $_POST['password'], $_POST['role']);
+// Check if POST data is set before using it
+$matric = isset($_POST['matric']) ? $_POST['matric'] : null;
+$name = isset($_POST['name']) ? $_POST['name'] : null;
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+$role = isset($_POST['role']) ? $_POST['role'] : null;
+
+if ($matric && $name && $password && $role) {
+    // Register the user using POST data
+    $user->createUser($matric, $name, $password, $role);
+} else {
+    echo "All fields are required.";
+}
 
 // Close the connection
 $db->close();
+?>
