a19cbef314d07d3c00f8324fe261e0a4aefb1aa9
TS.Hazim <killerzlife13@gmail.com>
2024-06-02 21:33:37 +0800

    initial

--- delete.php
+++ delete.php
@@ -3,17 +3,24 @@ include 'database.php';
 include 'User.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // Retrieve the matric value from the POST request
-    $matric = $_GET['matric'];
+    // Check if 'matric' is set in the GET request
+    if (isset($_GET['matric'])) {
+        // Retrieve the matric value from the GET request
+        $matric = $_GET['matric'];
 
-    // Create an instance of the Database class and get the connection
-    $database = new Database();
-    $db = $database->getConnection();
+        // Create an instance of the Database class and get the connection
+        $database = new Database();
+        $db = $database->getConnection();
 
-    // Create an instance of the User class
-    $user = new User($db);
-    $user->deleteUser($matric);
+        // Create an instance of the User class
+        $user = new User($db);
+        $user->deleteUser($matric);
 
-    // Close the connection
-    $db->close();
+        // Close the connection
+        $db->close();
+    } else {
+        // Handle the case when 'matric' is not provided in the GET request
+        echo "Matric value is required.";
+    }
 }
+?>
